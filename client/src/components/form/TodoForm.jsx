67b0df1864a71594d1e30bb9b4fb5a109f5e3b43
eugene94 <eugenekang94@gmail.com>
2019-05-18 12:25:30 +0900

    Style: TodoForm Component 디자인 수정
    Form.Input 형태로 모두 변경

--- client/src/components/form/TodoForm.jsx
+++ client/src/components/form/TodoForm.jsx
@@ -1,21 +1,8 @@
 import React, { Component } from 'react';
 import { Link } from 'react-router-dom';
 
-import './TodoForm.scss';
-
 import { DateTimeInput } from 'semantic-ui-calendar-react';
-import {
-  Grid,
-  Form,
-  Button,
-  Segment,
-  Radio,
-  Input,
-  Divider,
-  Checkbox,
-  Header,
-  TextArea
-} from 'semantic-ui-react';
+import { Grid, Form, Button, Segment, Divider } from 'semantic-ui-react';
 
 class TodoForm extends Component {
   render() {
@@ -27,47 +14,51 @@ class TodoForm extends Component {
           <Form size="large">
             <Segment stacked clearing>
               <Form.Field>
-                <Input name="title" placeholder="Title" value={title} onChange={handleChange} />
+                <Form.Input
+                  label="Title"
+                  name="title"
+                  id="form-input-control-title"
+                  placeholder="Todo를 만드세요."
+                  value={title}
+                  onChange={handleChange}
+                  required
+                />
               </Form.Field>
               <Form.Field>
-                <TextArea
+                <Form.TextArea
+                  label="Description"
                   name="content"
-                  placeholder="Description"
+                  id="form-input-control-content"
+                  placeholder="설명을 적어주세요..."
                   value={content}
                   onChange={handleChange}
                 />
               </Form.Field>
               <Divider section />
-              <Header as="h3">중요도</Header>
-              <Form.Field>
-                <Radio
+              <Form.Group grouped>
+                <label>Priority</label>
+                <Form.Radio
                   label="매우 중요"
                   name="priority"
                   value={1}
                   checked={priority === 1}
                   onChange={handleChange}
                 />
-              </Form.Field>
-              <Form.Field>
-                <Radio
+                <Form.Radio
                   label="중요"
                   name="priority"
                   value={2}
                   checked={priority === 2}
                   onChange={handleChange}
                 />
-              </Form.Field>
-              <Form.Field>
-                <Radio
+                <Form.Radio
                   label="보통"
                   name="priority"
                   value={3}
                   checked={priority === 3}
                   onChange={handleChange}
                 />
-              </Form.Field>
-              <Form.Field>
-                <Radio
+                <Form.Radio
                   label="중요하지 않음"
                   name="priority"
                   value={4}
@@ -75,12 +66,13 @@ class TodoForm extends Component {
                   onChange={handleChange}
                   color="red"
                 />
-              </Form.Field>
+              </Form.Group>
               <Divider section />
-              <Grid columns={2}>
-                <Grid.Column>
-                  <Header as="h3">마감기한</Header>
+              <Form.Group widths="equal">
+                <Form.Field>
                   <DateTimeInput
+                    label="마감기한"
+                    inlineLabel
                     name="deadline"
                     placeholder="Deadline"
                     value={deadline}
@@ -90,14 +82,17 @@ class TodoForm extends Component {
                     dateFormat="YYYY-MM-DD"
                     onChange={handleChange}
                   />
-                </Grid.Column>
-                <Grid.Column>
-                  <Header as="h3">완료</Header>
-                  <Form.Field>
-                    <Checkbox name="isDone" slider checked={isDone} onChange={onToggle} />
-                  </Form.Field>
-                </Grid.Column>
-              </Grid>
+                </Form.Field>
+                <Form.Field>
+                  <Form.Checkbox
+                    label={<label>완료</label>}
+                    name="isDone"
+                    slider
+                    checked={isDone}
+                    onChange={onToggle}
+                  />
+                </Form.Field>
+              </Form.Group>
             </Segment>
             <Button.Group floated="right">
               <Link to="/">
