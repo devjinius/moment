333f00eeb1b505497af6094d9fd1fca3dc3eca40
eugene94 <eugenekang94@gmail.com>
2019-05-18 14:38:57 +0900

    Feat: todo update 기능 구현

--- client/src/components/form/TodoForm.jsx
+++ client/src/components/form/TodoForm.jsx
@@ -2,107 +2,144 @@ import React, { Component } from 'react';
 import { Link } from 'react-router-dom';
 
 import { DateTimeInput } from 'semantic-ui-calendar-react';
-import { Grid, Form, Button, Segment, Divider } from 'semantic-ui-react';
+import { Grid, Form, Button, Segment, Divider, Message } from 'semantic-ui-react';
 
 class TodoForm extends Component {
   render() {
-    const { title, content, priority, isDone, deadline, handleChange, onToggle } = this.props;
+    const {
+      id,
+      title,
+      content,
+      priority,
+      checked,
+      deadline,
+      handleChange,
+      onToggle,
+      onSubmit,
+      success,
+      error
+    } = this.props;
+
+    const isLoding = id => (id === -1 ? true : false);
 
     return (
       <Grid>
-        <Grid.Column>
-          <Form size="large">
-            <Segment stacked clearing>
-              <Form.Field>
-                <Form.Input
-                  label="Title"
-                  name="title"
-                  id="form-input-control-title"
-                  placeholder="Todo를 만드세요."
-                  value={title}
-                  onChange={handleChange}
-                  required
-                />
-              </Form.Field>
-              <Form.Field>
-                <Form.TextArea
-                  label="Description"
-                  name="content"
-                  id="form-input-control-content"
-                  placeholder="설명을 적어주세요..."
-                  value={content}
-                  onChange={handleChange}
-                />
-              </Form.Field>
-              <Divider section />
-              <Form.Group grouped>
-                <label>Priority</label>
-                <Form.Radio
-                  label="매우 중요"
-                  name="priority"
-                  value={1}
-                  checked={priority === 1}
-                  onChange={handleChange}
-                />
-                <Form.Radio
-                  label="중요"
-                  name="priority"
-                  value={2}
-                  checked={priority === 2}
-                  onChange={handleChange}
-                />
-                <Form.Radio
-                  label="보통"
-                  name="priority"
-                  value={3}
-                  checked={priority === 3}
-                  onChange={handleChange}
-                />
-                <Form.Radio
-                  label="중요하지 않음"
-                  name="priority"
-                  value={4}
-                  checked={priority === 4}
-                  onChange={handleChange}
-                  color="red"
-                />
-              </Form.Group>
-              <Divider section />
-              <Form.Group widths="equal">
+        <Grid.Row>
+          <Grid.Column>
+            <Form size="large" loading={isLoding(id)} success={success} error={error}>
+              <Segment stacked clearing>
                 <Form.Field>
-                  <DateTimeInput
-                    label="마감기한"
-                    inlineLabel
-                    name="deadline"
-                    placeholder="Deadline"
-                    value={deadline}
-                    iconPosition="left"
-                    clearable
-                    closable
-                    dateFormat="YYYY-MM-DD"
+                  <Form.Input
+                    label="Title"
+                    name="title"
+                    id="form-input-control-title"
+                    placeholder="Todo를 만드세요."
+                    value={title}
+                    onChange={handleChange}
+                    required
+                    error={!title}
+                  />
+                </Form.Field>
+                <Form.Field>
+                  <Form.TextArea
+                    label="Description"
+                    name="content"
+                    id="form-input-control-content"
+                    placeholder="설명을 적어주세요..."
+                    value={content}
                     onChange={handleChange}
                   />
                 </Form.Field>
-                <Form.Field>
-                  <Form.Checkbox
-                    label={<label>완료</label>}
-                    name="isDone"
-                    slider
-                    checked={isDone}
-                    onChange={onToggle}
+                <Divider section />
+                <Form.Group grouped>
+                  <label>Priority</label>
+                  <Form.Radio
+                    label="매우 중요"
+                    name="priority"
+                    value={1}
+                    checked={priority === 1}
+                    onChange={handleChange}
                   />
-                </Form.Field>
-              </Form.Group>
-            </Segment>
+                  <Form.Radio
+                    label="중요"
+                    name="priority"
+                    value={2}
+                    checked={priority === 2}
+                    onChange={handleChange}
+                  />
+                  <Form.Radio
+                    label="보통"
+                    name="priority"
+                    value={3}
+                    checked={priority === 3}
+                    onChange={handleChange}
+                  />
+                  <Form.Radio
+                    label="중요하지 않음"
+                    name="priority"
+                    value={4}
+                    checked={priority === 4}
+                    onChange={handleChange}
+                    color="red"
+                  />
+                </Form.Group>
+                <Divider section />
+                <Form.Group widths="equal">
+                  <Form.Field>
+                    <DateTimeInput
+                      label="마감기한"
+                      inlineLabel
+                      name="deadline"
+                      placeholder="Deadline"
+                      value={deadline}
+                      iconPosition="left"
+                      clearable
+                      closable
+                      dateFormat="YYYY-MM-DD"
+                      onChange={handleChange}
+                    />
+                  </Form.Field>
+                  <Form.Field>
+                    <Form.Checkbox
+                      label={<label>완료</label>}
+                      name="checked"
+                      slider
+                      checked={checked}
+                      onChange={onToggle}
+                    />
+                  </Form.Field>
+                </Form.Group>
+              </Segment>
+            </Form>
+            {success ? (
+              <Message success header="수정완료" content="변경하신 사항이 모두 수정되었습니다." />
+            ) : (
+              <></>
+            )}
+            {error ? (
+              <Message
+                error
+                header="실패"
+                content="변경하신 사항이 수정되지 않았습니다. 관리자에게 문의해주세요(123번)"
+              />
+            ) : (
+              <></>
+            )}
+          </Grid.Column>
+        </Grid.Row>
+        <Grid.Row>
+          <Grid.Column>
             <Button.Group floated="right">
               <Link to="/">
-                <Button>취소</Button>
+                <Button>돌아가기</Button>
               </Link>
               <Button.Or />
-              <Button positive>수정하기</Button>
+              <Button positive onClick={onSubmit}>
+                수정하기
+              </Button>
             </Button.Group>
-          </Form>
-        </Grid.Column>
+          </Grid.Column>
+        </Grid.Row>
       </Grid>
     );
   }
--- client/src/pages/EditTodo.jsx
+++ client/src/pages/EditTodo.jsx
@@ -12,10 +12,20 @@ const Wrapper = styled.div`
 `;
 
 class EditTodo extends Component {
-  state = { id: -1, title: '', content: '', priority: 0, isDone: true, deadline: '' };
+  state = {
+    id: -1,
+    title: '',
+    content: '',
+    priority: 0,
+    checked: true,
+    deadline: '',
+    success: false,
+    error: false
+  };
 
   componentDidMount() {
     const { id } = this.props.match.params;
+
     ApiCommon.get(`/api/todo/${id}`).then(res => {
       const { data } = res.data;
       this.setState(data);
@@ -26,11 +36,34 @@ class EditTodo extends Component {
 
   onToggle = (e, { name, checked }) => this.setState({ [name]: checked });
 
+  onSubmit = e => {
+    const data = this.state;
+
+    ApiCommon.patch('/api/todo/', data.id, data).then(res => {
+      const { error, data } = res.data;
+
+      if (error) {
+        const errorState = { ...this.state, error: true };
+        this.setState(errorState);
+
+        return;
+      }
+
+      data.success = true;
+      this.setState(data);
+    });
+  };
+
   render() {
     return (
       <Wrapper>
         <Header as="h2">Edit Todo</Header>
-        <TodoForm {...this.state} handleChange={this.editHandleChange} onToggle={this.onToggle} />
+        <TodoForm
+          {...this.state}
+          handleChange={this.editHandleChange}
+          onToggle={this.onToggle}
+          onSubmit={this.onSubmit}
+        />
       </Wrapper>
     );
   }
